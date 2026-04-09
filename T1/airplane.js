552427641e7df9d428aa27ebae3df6dedac3ba07
henrique <henrique.silveira@estudante.ufjf.br>
2026-04-09 17:05:50 -0300

    Definindo estruturas básicas e transformando em funções

--- T1/airplane.js
+++ T1/airplane.js
@@ -7,7 +7,6 @@ import {
     setDefaultMaterial,
     InfoBox,
     onWindowResize,
-    createGroundPlaneXZ
 } from "../libs/util/util.js";
 
 let scene, renderer, camera, material, light, orbit; // Initial variables
@@ -29,80 +28,10 @@ scene.add(axesHelper);
 
 
 
-
-// Materiais
-const materialAzul = setDefaultMaterial("rgb(23,62,125)");
-const materialAmarelo = setDefaultMaterial("rgb(194,140,39)")
-
-// Corpo
-// Usando CylinderGeometry com bases distintas
-const geometriaCilindro = new THREE.CylinderGeometry(0.9, 0.5, 1, 32, 1, false, Math.PI/6);
-const corpo = new THREE.Mesh(geometriaCilindro, materialAzul);
-corpo.scale.set(2.3, 13, 2);
-corpo.rotation.y = Math.PI;
-corpo.rotation.x = Math.PI / 2;
-
-// Asa frontal
-// Usando SphereGeometry achatado
-const geometriaEsfera = new THREE.SphereGeometry();
-const asa = new THREE.Mesh(geometriaEsfera, materialAzul);
-asa.scale.set(10, 0.5, 1.5);
-asa.position.set(0, 0, 0.5);
-
-// Asa traseira
-// Usando SphereGeometry achatada, igual à asa principal
-const geometriaCaudaHoriz = new THREE.SphereGeometry();
-const caudaHorizontal = new THREE.Mesh(geometriaCaudaHoriz, materialAmarelo);
-caudaHorizontal.scale.set(3.5, 0.4, 1);
-caudaHorizontal.position.set(0, 0, -5.5); // Posicionado na parte de trás do cilindro
-
-// Cauda (Leme)
-// Usando BoxGeometry para fazer uma barbatana direcional
-const geometriaCaudaVert = new THREE.BoxGeometry(0.8, 0.8, 0.8);
-const caudaVertical = new THREE.Mesh(geometriaCaudaVert, materialAmarelo);
-caudaVertical.scale.set(0.3, 2, 1.9);
-caudaVertical.position.set(0, 1, -5.5); // Acima da asa traseira
-caudaVertical.rotation.x = Math.PI / 8;   // Leve inclinação para trás para dar estilo
-
-// Cabine
-// Uma meia-esfera alongada em cima do corpo.
-const geometriaCabine = new THREE.SphereGeometry(0.8);
-const cabine = new THREE.Mesh(geometriaCabine, materialAmarelo);
-cabine.scale.set(1.2, 1.2, 2.5);
-cabine.position.set(0, 1.5, 0.6); // Na parte superior, ligeiramente à frente
-
-// Hélice
-// Usando BoxGeometry na parte frontal do avião
-const geometriaHelice = new THREE.BoxGeometry(1, 1, 1);
-const helice = new THREE.Mesh(geometriaHelice, materialAmarelo);
-helice.scale.set(5, 0.4, 0.1);
-helice.position.set(0, 0, 6.6); // Bem na ponta frontal do cilindro
-
-// Miolo da Hélice
-// Usando SphereGeometry no centro da hélice
-const geometriaMiolo = new THREE.SphereGeometry();
-const miolo = new THREE.Mesh(geometriaMiolo, materialAzul);
-miolo.scale.set(0.6, 0.6, 0.6);
-miolo.position.set(0, 0, 6.7);
-
-// Arco
-// Usando TorusGeometry para dar sensação de movimento na hélice
-const geometriaArco = new THREE.TorusGeometry(1.85, 0.14);
-const arco = new THREE.Mesh(geometriaArco, materialAmarelo);
-arco.scale.set(1.3, 1.3, 0.01);
-arco.position.set(0, 0, 6.6);
-
-// Adicionando estruturas no corpo do avião
-scene.add(caudaHorizontal);
-scene.add(caudaVertical);
-scene.add(cabine);
-scene.add(helice);
-scene.add(miolo);
-scene.add(arco);
-scene.add(asa);
+const aviao = criarAviao();
 
 // Adicionando avião na cena
-scene.add(corpo);
+scene.add(aviao);
 
 
 
@@ -122,4 +51,80 @@ render();
 function render() {
     requestAnimationFrame(render);
     renderer.render(scene, camera) // Render scene
+}
+
+function criarAviao() {
+    // Materiais
+    const materialAzul = setDefaultMaterial("rgb(23,62,125)");
+    const materialAmarelo = setDefaultMaterial("rgb(194,140,39)")
+
+    // Corpo
+    // Usando CylinderGeometry com bases distintas
+    const geometriaCilindro = new THREE.CylinderGeometry(2, 1, 13);
+    const corpo = new THREE.Mesh(geometriaCilindro, materialAzul);
+    corpo.rotation.x = Math.PI / 2;
+
+    // Asa frontal
+    // Usando SphereGeometry achatado
+    const geometriaEsfera = new THREE.SphereGeometry();
+    const asa = new THREE.Mesh(geometriaEsfera, materialAzul);
+    asa.scale.set(10, 0.5, 1.5);
+    asa.rotation.x = -Math.PI / 2;
+    corpo.add(asa);
+
+    // Asa traseira
+    // Usando SphereGeometry achatada, igual à asa principal
+    const geometriaCaudaHoriz = new THREE.SphereGeometry();
+    const caudaHorizontal = new THREE.Mesh(geometriaCaudaHoriz, materialAmarelo);
+    caudaHorizontal.scale.set(3.5, 0.4, 1);
+    caudaHorizontal.position.set(0, -5.5, 0);
+    caudaHorizontal.rotation.x = -Math.PI / 2;
+    corpo.add(caudaHorizontal);
+
+    // Cauda (Leme)
+    // Usando BoxGeometry para fazer uma barbatana direcional
+    const geometriaCaudaVert = new THREE.BoxGeometry(0.8, 0.8, 0.8);
+    const caudaVertical = new THREE.Mesh(geometriaCaudaVert, materialAmarelo);
+    caudaVertical.scale.set(0.3, 2, 1.9);
+    caudaVertical.position.set(0, -5.5, -1);
+    caudaVertical.rotation.x = -Math.PI / 8;
+    corpo.add(caudaVertical);
+
+    // Cabine
+    // Uma meia-esfera alongada em cima do corpo.
+    const geometriaCabine = new THREE.SphereGeometry(0.8);
+    const cabine = new THREE.Mesh(geometriaCabine, materialAmarelo);
+    cabine.scale.set(1.2, 1.2, 2.5);
+    cabine.position.set(0, 0, -1.5);
+    cabine.rotation.x = -Math.PI / 2;
+    corpo.add(cabine);
+
+    // Hélice
+    // Usando BoxGeometry na parte frontal do avião
+    const geometriaHelice = new THREE.BoxGeometry(1, 1, 1);
+    const helice = new THREE.Mesh(geometriaHelice, materialAmarelo);
+    helice.scale.set(5, 0.4, 0.1);
+    helice.position.set(0, 6.6, 0);
+    helice.rotation.x = -Math.PI / 2;
+    corpo.add(helice);
+
+    // Miolo da Hélice
+    // Usando SphereGeometry no centro da hélice
+    const geometriaMiolo = new THREE.SphereGeometry();
+    const miolo = new THREE.Mesh(geometriaMiolo, materialAzul);
+    miolo.scale.set(0.6, 0.6, 0.6);
+    miolo.position.set(0, 6.7, 0);
+    miolo.rotation.x = -Math.PI / 2;
+    corpo.add(miolo);
+
+    // Arco
+    // Usando TorusGeometry para dar sensação de movimento na hélice
+    const geometriaArco = new THREE.TorusGeometry(1.85, 0.14);
+    const arco = new THREE.Mesh(geometriaArco, materialAmarelo);
+    arco.scale.set(1.3, 1.3, 0.01);
+    arco.position.set(0, 6.6, 0);
+    arco.rotation.x = -Math.PI / 2;
+    corpo.add(arco);
+
+    return corpo;
 }
--- T1/tree.js
+++ T1/tree.js
@@ -28,48 +28,12 @@ window.addEventListener('resize', function () {
 let axesHelper = new THREE.AxesHelper(12);
 scene.add(axesHelper);
 
-// create the ground plane
-// let plane = createGroundPlaneXZ(20, 20);
-// scene.add(plane);
-
-const materialVerde = setDefaultMaterial("green");
-const materialMarrom = setDefaultMaterial("brown");
-
-const cylinderGeometry = new THREE.CylinderGeometry(1.8, 2, 2, 16);
-const cilindro = new THREE.Mesh(cylinderGeometry, materialMarrom);
-
-const coneGeometry1 = new THREE.ConeGeometry(3, 4, 32);
-const coneGeometry2 = new THREE.ConeGeometry(4, 5, 32);
-const coneGeometry3 = new THREE.ConeGeometry(4.5, 5.5, 32);
-
-const cone1 = new THREE.Mesh(coneGeometry1, materialVerde);
-const cone2 = new THREE.Mesh(coneGeometry2, materialVerde);
-const cone3 = new THREE.Mesh(coneGeometry3, materialVerde);
-
-const alturaCilindro = cilindro.geometry.parameters.height;
-const alturaCone1 = coneGeometry1.parameters.height;
-const alturaCone2 = coneGeometry2.parameters.height;
-const alturaCone3 = coneGeometry3.parameters.height;
-
-cilindro.position.set(0, alturaCilindro / 2, 0);
-
-const posY3 = alturaCilindro / 2 + alturaCone3 / 2;
-cone3.position.set(0, posY3, 0);
-
-const posY2 = posY3 + alturaCone3 / 2;
-cone2.position.set(0, posY2, 0);
-
-const posY1 = posY2 + alturaCone2 / 2;
-cone1.position.set(0, posY1, 0);
-
-cilindro.add(cone1);
-cilindro.add(cone2);
-cilindro.add(cone3);
 
 
+const arvore = criarArvore();
+scene.add(arvore);
 
 
-scene.add(cilindro);
 
 // Use this to show information onscreen
 let controls = new InfoBox();
@@ -86,4 +50,45 @@ render();
 function render() {
     requestAnimationFrame(render);
     renderer.render(scene, camera) // Render scene
+}
+
+function criarArvore() {
+    // Materiais
+    const materialVerde = setDefaultMaterial("green");
+    const materialMarrom = setDefaultMaterial("brown");
+
+    // Tronco da árvore
+    const cylinderGeometry = new THREE.CylinderGeometry(1.8, 2, 2, 16);
+    const tronco = new THREE.Mesh(cylinderGeometry, materialMarrom);
+
+    // Topo da árvore (Folhas)
+    const coneGeometry1 = new THREE.ConeGeometry(3, 4, 32);
+    const coneGeometry2 = new THREE.ConeGeometry(4, 5, 32);
+    const coneGeometry3 = new THREE.ConeGeometry(4.5, 5.5, 32);
+
+    const cone1 = new THREE.Mesh(coneGeometry1, materialVerde);
+    const cone2 = new THREE.Mesh(coneGeometry2, materialVerde);
+    const cone3 = new THREE.Mesh(coneGeometry3, materialVerde);
+
+    const alturaCilindro = tronco.geometry.parameters.height;
+    const alturaCone2 = coneGeometry2.parameters.height;
+    const alturaCone3 = coneGeometry3.parameters.height;
+
+    tronco.position.set(0, alturaCilindro / 2, 0);
+
+    const posY3 = alturaCilindro / 2 + alturaCone3 / 2;
+    cone3.position.set(0, posY3, 0);
+
+    const posY2 = posY3 + alturaCone3 / 2;
+    cone2.position.set(0, posY2, 0);
+
+    const posY1 = posY2 + alturaCone2 / 2;
+    cone1.position.set(0, posY1, 0);
+
+    // Adicionando folhas no tronco
+    tronco.add(cone1);
+    tronco.add(cone2);
+    tronco.add(cone3);
+
+    return tronco;
 }
