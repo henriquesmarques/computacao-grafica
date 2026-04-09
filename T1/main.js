e29004cd7a80eff23bbd97ee15069a66914f6dd5
henrique <henrique.silveira@estudante.ufjf.br>
2026-04-09 20:24:57 -0300

    Criando interface

--- T1/main.js
+++ T1/main.js
@@ -1,12 +1,15 @@
 import * as THREE from 'three';
 import {OrbitControls} from '../build/jsm/controls/OrbitControls.js';
-import { InfoBox, initRenderer, initCamera, initDefaultBasicLight, onWindowResize, createGroundPlaneXZ } from "../libs/util/util.js";
-import Stats from       '../build/jsm/libs/stats.module.js';
+import { initRenderer, initCamera, initDefaultBasicLight, onWindowResize, createGroundPlaneXZ } from "../libs/util/util.js";
+import Stats from '../build/jsm/libs/stats.module.js';
+import GUI from '../libs/util/dat.gui.module.js'
 import { criarArvore, criarAviao } from "./util.js";
 
 const scene = new THREE.Scene();
 const renderer = initRenderer();
 initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+let animationOn = true;
+
 
 // Câmera
 const camera = initCamera(new THREE.Vector3(0, 15, 30));
@@ -19,8 +22,9 @@ new OrbitControls(camera, renderer.domElement);
 window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
 // Fog (Névoa)
+let valorFOG = 100;
 const baseColor = "rgb(175, 200, 220)"; // a cor do FOG deve ser a mesma do background
-scene.fog = new THREE.Fog(baseColor, 1, 100);
+scene.fog = new THREE.Fog(baseColor, 1, valorFOG);
 renderer.setClearColor(baseColor);
 
 // Status (FPS)
@@ -42,22 +46,34 @@ aviao.add(eixo);
 scene.add(aviao);
 
 
-
-
-// Use this to show information onscreen
-const controls = new InfoBox();
-controls.add("Basic Scene");
-controls.addParagraph();
-controls.add("Use mouse to interact:");
-controls.add("* Left button to rotate");
-controls.add("* Right button to translate (pan)");
-controls.add("* Scroll to zoom in/out.");
-controls.show();
-
+buildInterface();
 render();
+
+function buildInterface() {
+    // Controles
+    const controls = new function ()
+    {
+        this.onChangeAnimation = function(){
+            animationOn = !animationOn;
+        };
+        this.fog = valorFOG;
+
+        this.changeFOG = function(){
+            valorFOG = this.fog;
+            scene.fog.far = this.fog;
+        };
+    };
+
+    // Interface
+    const gui = new GUI();
+    gui.add(controls, 'onChangeAnimation',true).name("Animation On/Off");
+    gui.add(controls, 'fog', 10, 100)
+       .onChange(function(e) { controls.changeFOG() })
+       .name("Change FOG");
+}
 
 function render() {
     requestAnimationFrame(render);
-    renderer.render(scene, camera) // Render scene
     stats.update();
+    renderer.render(scene, camera) // Render scene
 }
