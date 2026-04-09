19ee3c18e1cc3b8e537103ae6dcbb0e64b761ff0
henrique <henrique.silveira@estudante.ufjf.br>
2026-04-09 20:00:56 -0300

    Adicionando visualizador de FPS na cena

--- T1/main.js
+++ T1/main.js
@@ -1,54 +1,51 @@
 import * as THREE from 'three';
 import {OrbitControls} from '../build/jsm/controls/OrbitControls.js';
-import {
-    InfoBox,
-    initRenderer,
-    initCamera,
-    initDefaultBasicLight,
-    onWindowResize,
-    createGroundPlaneXZ
-} from "../libs/util/util.js";
-
+import { InfoBox, initRenderer, initCamera, initDefaultBasicLight, onWindowResize, createGroundPlaneXZ } from "../libs/util/util.js";
+import Stats from       '../build/jsm/libs/stats.module.js';
 import { criarArvore, criarAviao } from "./util.js";
 
-let scene, renderer, camera, light, orbit; // Initial variables
-scene = new THREE.Scene();    // Create main scene
-renderer = initRenderer();    // Init a basic renderer
-light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
-camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
-orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
+const scene = new THREE.Scene();
+const renderer = initRenderer();
+initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+
+// Câmera
+const camera = initCamera(new THREE.Vector3(0, 15, 30));
+scene.add(camera);
+
+// Enable mouse rotation, pan, zoom etc.
+new OrbitControls(camera, renderer.domElement);
 
 // Listen window size changes
-window.addEventListener('resize', function () {
-    onWindowResize(camera, renderer)
-}, false);
+window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
 // Fog (Névoa)
-const baseColor = "rgb(175, 200, 220)"; // It's important the fog color is the same as the background
+const baseColor = "rgb(175, 200, 220)"; // a cor do FOG deve ser a mesma do background
 scene.fog = new THREE.Fog(baseColor, 1, 100);
 renderer.setClearColor(baseColor);
 
+// Status (FPS)
+const stats = new Stats();
+document.getElementById("webgl-output").appendChild(stats.domElement);
 
-const ground = createGroundPlaneXZ(150, 150);
+// Plano
+const plano = createGroundPlaneXZ(150, 150);
+scene.add(plano);
+
+
+
+// Avião
 const eixo = new THREE.AxesHelper(12);
 const aviao = criarAviao();
-
-
-
 aviao.position.set(0, 5, 0);
 
-
-
 aviao.add(eixo);
-
-scene.add(camera);
-scene.add(ground);
 scene.add(aviao);
 
 
 
+
 // Use this to show information onscreen
-let controls = new InfoBox();
+const controls = new InfoBox();
 controls.add("Basic Scene");
 controls.addParagraph();
 controls.add("Use mouse to interact:");
@@ -62,4 +59,5 @@ render();
 function render() {
     requestAnimationFrame(render);
     renderer.render(scene, camera) // Render scene
+    stats.update();
 }
