a728c932c96954b8e6234ebf9472c71aa4c8a417
Karine <karine.gomes@estudante.ufjf.br>
2026-06-23 22:54:45 -0300

    teste14

--- T3/main.js
+++ T3/main.js
@@ -630,28 +630,29 @@ function configurarJanela() {
         const manager = nipplejs.create({
             zone: joystickZone,
             mode: 'static',
-            position: {left: '60px',bottom: '60px'},
+            position: {
+                left: '60px',
+                bottom: '60px'
+            },
             color: '#85ff8d',
             size: 100
         });
-
         manager.on('move', function(evt, data){
             if (!data.vector) return;
             const velocidade = 0.8;
             // move a mira REAL
-            mira.position.x += data.vector.x * velocidade;
-            mira.position.y += data.vector.y * velocidade;
+            mira.position.x +=data.vector.x * velocidade;
+            mira.position.y +=data.vector.y * velocidade;
             // limites
             mira.position.x = Math.max(-limiteXDinamico,Math.min(limiteXDinamico,mira.position.x));
-            mira.position.y = Math.max(4,Math.min(25, mira.position.y));
+            mira.position.y = Math.max(4,Math.min(25,mira.position.y));
             // força o avião a enxergar a nova posição
             mira.updateMatrixWorld();
-            mira.position.z = aviao.position.z - 30;
             // tiro contínuo
             mousePressionado = true;
         });
         manager.on('end', function(){
-        mousePressionado = false;
+            mousePressionado = false;
         });
     }
 } 
