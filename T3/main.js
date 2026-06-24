41432d736426dd030b795f36fbf08da501811255
Karine <karine.gomes@estudante.ufjf.br>
2026-06-23 23:12:15 -0300

    teste19

--- T3/main.js
+++ T3/main.js
@@ -344,7 +344,10 @@ function renderizar() {
     const deltaTime = relogio.getDelta();
     if (animacaoAtiva) {
         // Atualização de Posições e Controles
-        atualizarMira(raycaster, mouse, camera, paredeInvisivel, mira, limiteXDinamico);
+        if (!('ontouchstart' in window)) {
+            atualizarMira(raycaster, mouse, camera, paredeInvisivel, mira, limiteXDinamico);
+
+}
         atualizarCamera(aviao, mira, camera, paredeInvisivel, velocidadeDeslocamento);
 
         // Animações e Cenário
